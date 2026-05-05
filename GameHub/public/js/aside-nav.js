5126e5a7dbe8ba1645373d87ab76b08a4e38cb4e
Freyr <mail@freyrvl.dev>
2026-05-05 15:32:34 +0000

    Mobile friendly side nav, setup

---
+++ GameHub/public/js/aside-nav.js
@@ -0,0 +1,14 @@
+const asideNavWrapper = document.querySelector(".aside-nav-wrapper");
+const menuButton = document.querySelector(".menu-button");
+const hiddenToggle = document.querySelector(".hidden");
+
+menuButton.addEventListener("click", event => {
+    if(menuButton.classList.contains("fa-bars")){
+        menuButton.classList.replace("fa-bars", "fa-x");
+    }
+    else{
+        menuButton.classList.replace("fa-x", "fa-bars");
+    }
+
+    hiddenToggle.classList.toggle("hidden");
+})
