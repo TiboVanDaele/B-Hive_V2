02cb37efd4a63f366edc42426f8749cfb29657d2
Freyr <mail@freyrvl.dev>
2026-05-06 07:44:42 +0000

    Temporary version commit before trying out new layouts

--- GameHub/public/js/aside-nav.js
+++ GameHub/public/js/aside-nav.js
@@ -5,10 +5,11 @@ const hiddenToggle = document.querySelector(".hidden");
 menuButton.addEventListener("click", event => {
     if(menuButton.classList.contains("fa-bars")){
         menuButton.classList.replace("fa-bars", "fa-x");
+        
     }
     else{
         menuButton.classList.replace("fa-x", "fa-bars");
     }
-
     hiddenToggle.classList.toggle("hidden");
+    asideNavWrapper.classList.toggle("open");
 })
