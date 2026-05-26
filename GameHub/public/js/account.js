15336e1d8b029364990b27ed1ff966145a6448a3
Jonas Deschaek <jonasdeschaek000@gmail.com>
2026-05-26 16:17:46 +0000

    current game gefixed en kleine styling issue op index gefixed

--- GameHub/public/js/account.js
+++ GameHub/public/js/account.js
@@ -12,5 +12,17 @@ function hideEditForm() {
     editBtn.style.display = "";
 }
 
+const removeBtn = document.getElementById("removeCurrentBtn");
+if (removeBtn) {
+    removeBtn.addEventListener("click", async () => {
+        const res = await fetch("/api/users/current-game", {
+            method: "DELETE"
+        });
+        const data = await res.json();
+        if (data.success) {
+            document.querySelector(".current-game-card").remove();
+        }
+    });
+}
 editBtn.addEventListener("click", showEditForm);
 cancelBtn.addEventListener("click", hideEditForm);
